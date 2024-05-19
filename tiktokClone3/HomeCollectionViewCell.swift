3b44220aa19eb0275c5e41efb591ef49cef6ce73
Mahmut Başcı <mahocb@Mahmut-Dizustu-Bilgisayar.local>
2024-05-19 11:41:42 +0300

    profile header tasarımı

--- tiktokClone3/HomeCollectionViewCell.swift
+++ tiktokClone3/HomeCollectionViewCell.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postVideo: UIImageView!
     @IBOutlet weak var avatar: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         avatar.layer.cornerRadius = 55/2
